c8a89798a7d14dfa57c54b2e30d6cda2c332d335
jordgreenfield <79856478+jordgreenfield@users.noreply.github.com>
2022-01-08 23:36:24 +0900

    Add files via upload

---
+++ MegaBytesConverter/src/main.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class main {
+    public static void main(String[] args) {
+
+        System.out.print("Convert kilobyte to megabyte here: ");
+        Scanner s = new Scanner(System.in);
+        megaBytesConverter.printMegaBytesAndKiloBytes(s.nextInt());
+
+
+    }
+}
